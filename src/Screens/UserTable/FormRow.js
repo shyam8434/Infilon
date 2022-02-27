247c97b8c1a3b9aed9ad610257a0de91992118d7
shyam <shyam.kr.pra@gmail.com>
2022-02-27 14:17:07 +0530

    -- Implemented edit functionality

--- src/Screens/UserTable/FormRow.js
+++ src/Screens/UserTable/FormRow.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Button, Grid, Paper, styled } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Grid, Paper, styled, TextField } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { updateUser } from "../../AppConfig/redux/action/common";
 import { useDispatch, useSelector } from "react-redux";
+import { Edit, Save } from "@mui/icons-material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -10,9 +11,13 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.secondary,
+  height: "75%",
 }));
 
 const FormRow = ({ user }) => {
+  const [toggleEdit, setToggleEdit] = useState(false);
+  const [editData, setEditData] = useState(user);
+
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.common.users);
 
@@ -21,18 +26,65 @@ const FormRow = ({ user }) => {
     dispatch(updateUser(tempData));
   };
 
+  const handleOnEdit = (condition) => {
+    setToggleEdit(!toggleEdit);
+    if (condition) {
+        var tempData = [...userData]
+        tempData.forEach(item => {
+            if(item.id === user.id){
+                item.first_name = editData.first_name
+                item.last_name = editData.last_name
+                item.email = editData.email
+            }
+        })
+      dispatch(updateUser(tempData));
+    }
+  };
+
+  const handleEditText = (event) => {
+    var tempData = { ...editData };
+    tempData[event.target.id] = event.target.value;
+    setEditData(tempData);
+  };
+
   return (
     <React.Fragment>
       <Grid item xs={3}>
-        <Item>{user.first_name}</Item>
+        <Item>
+          {toggleEdit ? (
+            <TextField id="first_name" value={editData.first_name} onChange={handleEditText}/>
+          ) : (
+            user.first_name
+          )}
+        </Item>
       </Grid>
       <Grid item xs={3}>
-        <Item>{user.last_name}</Item>
+        <Item>
+          {toggleEdit ? (
+            <TextField id="last_name" value={editData.last_name} onChange={handleEditText}/>
+          ) : (
+            user.last_name
+          )}
+        </Item>
       </Grid>
       <Grid item xs={4}>
-        <Item>{user.email}</Item>
+        <Item>
+          {toggleEdit ? (
+            <TextField id="email" value={editData.email} onChange={handleEditText}/>
+          ) : (
+            user.email
+          )}
+        </Item>
       </Grid>
       <Grid item xs={2}>
+        <Button
+          variant="outlined"
+          style={{ backgroundColor: toggleEdit ? "aliceblue" : "transparent" }}
+          onClick={() => handleOnEdit(toggleEdit)}
+          startIcon={toggleEdit ? <Save /> : <Edit />}
+        >
+          {toggleEdit ? "Save" : "Edit"}
+        </Button>
         <Button
           variant="outlined"
           onClick={() => handleDelete(user)}
